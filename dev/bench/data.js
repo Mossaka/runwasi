9487212c7c2a740aff769fa6b3ac467db0ada603
github-action-benchmark <github@users.noreply.github.com>
2025-03-23 00:51:59 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for f3481703dd0d4b6b2ff9c72c4fd3ff54e2cd372b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1742690889285,
+  "lastUpdate": 1742691119594,
   "repoUrl": "https://github.com/Mossaka/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -1044,6 +1044,52 @@ window.BENCHMARK_DATA = {
             "value": 84100198,
             "range": "± 1854535",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jorge Prendes",
+            "username": "jprendes",
+            "email": "jorge.prendes@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "f3481703dd0d4b6b2ff9c72c4fd3ff54e2cd372b",
+          "message": "Merge pull request #877 from CaptainVincent/wasmedge-enable-plugin\n\nAdd plugin support to WasmEdge runtime",
+          "timestamp": "2025-03-12T23:23:48Z",
+          "url": "https://github.com/Mossaka/runwasi/commit/f3481703dd0d4b6b2ff9c72c4fd3ff54e2cd372b"
+        },
+        "date": 1742691118923,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15404,
+            "unit": "kB",
+            "extra": "shim: 12544 kB\nzygote: 2860 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 66384,
+            "unit": "kB",
+            "extra": "shim: 53760 kB\nzygote: 12624 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 18824,
+            "unit": "kB",
+            "extra": "shim: 15232 kB\nzygote: 3592 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 20156,
+            "unit": "kB",
+            "extra": "shim: 16640 kB\nzygote: 3516 kB"
           }
         ]
       }
