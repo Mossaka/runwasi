0df525d9c8c538a82e716007fa8f43aede9709a3
github-action-benchmark <github@users.noreply.github.com>
2026-02-19 01:07:10 +0000

    add Criterion.rs Benchmark (customSmallerIsBetter) benchmark result for aee137d4c33294951b4e6927fae6e59a14ed3bf8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771380499257,
+  "lastUpdate": 1771463230329,
   "repoUrl": "https://github.com/Mossaka/runwasi",
   "entries": {
     "Criterion.rs Benchmark": [
@@ -39556,6 +39556,52 @@ window.BENCHMARK_DATA = {
             "value": 295949879,
             "range": "± 6832098",
             "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Jiaxiao Zhou",
+            "username": "Mossaka",
+            "email": "duibao55328@gmail.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "aee137d4c33294951b4e6927fae6e59a14ed3bf8",
+          "message": "Merge pull request #949 from Mossaka/fix-release-pipeline-shimkit3\n\nRELEASE: update condition for crates.io ownership check",
+          "timestamp": "2025-03-26T18:06:04Z",
+          "url": "https://github.com/Mossaka/runwasi/commit/aee137d4c33294951b4e6927fae6e59a14ed3bf8"
+        },
+        "date": 1771463222070,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "wamr/memory-usage",
+            "value": 15548,
+            "unit": "kB",
+            "extra": "shim: 12408 kB\nzygote: 3140 kB"
+          },
+          {
+            "name": "wasmedge/memory-usage",
+            "value": 66816,
+            "unit": "kB",
+            "extra": "shim: 54124 kB\nzygote: 12692 kB"
+          },
+          {
+            "name": "wasmer/memory-usage",
+            "value": 19136,
+            "unit": "kB",
+            "extra": "shim: 15436 kB\nzygote: 3700 kB"
+          },
+          {
+            "name": "wasmtime/memory-usage",
+            "value": 20108,
+            "unit": "kB",
+            "extra": "shim: 16420 kB\nzygote: 3688 kB"
           }
         ]
       }
